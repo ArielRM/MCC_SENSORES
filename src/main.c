2c2ccd8c43a2ce6944883cd29c5ab0aadeac6e27
Raquel <sumeragi_86_subaru@hotmail.com>
2018-12-07 11:40:39 -0200

    simple_main

--- src/main.c
+++ src/main.c
@@ -1,27 +1,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
-
-#include "mbrtu.h"
 
 int main(void)
 {
-	mbrtu_init();
-	sei();
-
 	DDRB |= (1 << PB5);
 
-	mbrtu_send(0x15, 0x01, 0xAA);
 	while (1)
 	{
-		uint8_t frame[8];
-		if (!mbrtu_get(frame))
-		{
-			if (frame[3] == 1 )
-				PORTB |= (1 << PB5);
-			else
-				PORTB &= ~(1 << PB5);
-		}
+		PORTB ^= (1 << PB5);
+		_delay_ms(1000);
 	}
 
 	return 0;
